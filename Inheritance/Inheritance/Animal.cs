1a04218ae61f23a27394d07b03a4d12e7144964e
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-01-27 11:10:50 +0530

    Adding Inheritance Example Programs

---
+++ Inheritance/Inheritance/Animal.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Animal
+    {
+        public int weight;
+        public string color;
+        public string name;
+         
+        public void Sound()
+        {
+            Console.WriteLine("Sound..");
+        }
+        public void Eat()
+        {
+            Console.WriteLine("Eating...");
+        }
+        public class Dog : Animal
+        { 
+        public void Weep()
+        {
+            Console.WriteLine("Weeping...");
+        }
+            public class BabyDog : Animal
+            {
+                public void Eat()
+                {
+                    Console.WriteLine("Eating...");
+                }
+
+                public static void Main()
+                {
+                    BabyDog b = new BabyDog();
+                    b.Sound();
+                    b.Weep();
+                    b,Eate();
+
+                }
+
+            }
+        }
+    }
+}
---
+++ Inheritance/Inheritance/Mobile.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class Mobile
+    {
+        public int price;
+        public string name;
+        public string color;
+
+        public void Chatting()
+        {
+            Console.WriteLine(" Chatting...");
+        }
+        public void Calling()
+        {
+            Console.WriteLine("Calling...");
+        }
+        public void Music()
+        {
+            Console.WriteLine("Music....");
+        }
+        public class Inheritance
+        {
+            public static void Main()
+            {
+                Mobile m1 = new Mobile();
+                //properties
+                m1.price = 200000;
+                m1.name = "Poco C3";
+                m1.color = "Gray";
+                Console.WriteLine(m1.price);
+                Console.WriteLine(m1.color);
+                Console.WriteLine(m1.name);
+
+
+                //behaviour
+                m1.Chatting();
+                m1.Calling();
+                m1.Music();
+
+            }
+        }
+    }
+}
+    
+
